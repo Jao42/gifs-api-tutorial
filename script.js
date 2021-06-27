3b43bc21b1c4901a6a43328eeabade835043fb08
Jao42 <joaovictormedeiros41@gmail.com>
2021-06-27 20:56:06 -0300

    first version

--- script.js
+++ script.js
@@ -5,26 +5,38 @@ const termInput = document.querySelector('.search input')
 const termSubmitButton = document.querySelector('.search button')
 const buttons = document.querySelectorAll('button')
 
-let searchTerm = 'cats'
+const key = '0g9MebyVxmWGUhwVDxLPCHJ9Vqci9gCk'  
+const apiLink = 'https://api.giphy.com/v1/gifs/translate?api_key='
+  + key +
+  '&s='
 
+let searchTerm = 'cats'
+let fallbackSearchTerm
 
 async function getGifUrl(searchTerm) {
-   link = await fetch('https://api.giphy.com/v1/gifs/translate?api_key=0g9MebyVxmWGUhwVDxLPCHJ9Vqci9gCk&s='+searchTerm, {
+  link = await fetch(
+  apiLink + searchTerm, {
     mode: 'cors'
     })
     .then((res, err) => {
       if (err) {
-        console.log(err)
+        return err
       }
       else {
         data = res.json()
         return data 
       }
     })
-    .then(data => {
-      url = data['data']['images']['downsized_medium']['url']
-      return url
+    .then((data) => {
+      if (data['data']['images'] === undefined) {
+        throw new Error("ImageNotFound")
+      }
+      else {
+        url = data['data']['images']['downsized_medium']['url']
+        return url
+      }
     })
+
   return link
 }
 
@@ -32,21 +44,28 @@ getGifUrl(searchTerm).then((url => catImg.src = url))
 
 termSubmitButton.addEventListener('click', () => {
   if (termInput.value) {
+    fallbackSearchTerm = searchTerm
     searchTerm = termInput.value
-  }
-  else {
-    return false
   }
 })
 
 buttons.forEach((button) => {
   button.addEventListener('click', () => {
     loading.textContent = 'Loading...'
-    getGifUrl(searchTerm).then((url) => {
-      catImg.src = url
-      loading.textContent = ''
-    })
+    getGifUrl(searchTerm)
+      .then((url) => { 
+        catImg.src = url
+        loading.textContent = ''
+      })
+      .catch((err) => {
+        if (err.message === 'ImageNotFound') {
+          loading.textContent = 'We can\'t find a gif with this term :(.' +
+            ' Try search another thing :)'
+          searchTerm = fallbackSearchTerm
+        }
+        else {
+          loading.textContent = 'A thing not expected occurs... :/'
+        }
+      })
   })
 })
-
-
